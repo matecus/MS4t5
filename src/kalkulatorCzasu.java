6a5df4f5c7ef594d716eeddf490335101051b7b0
matecus <125434606+matecus@users.noreply.github.com>
2023-03-01 19:22:55 +0100

    Update kalkulatorCzasu.java

--- src/kalkulatorCzasu.java
+++ src/kalkulatorCzasu.java
@@ -1,26 +1,21 @@
 import java.util.Scanner;
-public class kalkulatorCzasu {
+public class kalkulatorCzasu
+{
     public static void main(String[] args) {
         Scanner klawiatura = new Scanner(System.in);
-
-        System.out.println("Wprowadź liczbę sekund: ");
-        double sekundy = klawiatura.nextDouble();
-        klawiatura.nextLine();
-
-        final int min = 60;
-        final int hour = 3600;
-        final int day = 86400;
-        if (sekundy >=min) {
-            double min2 = sekundy/min;
-            double sek2 = sekundy/min;
-            System.out.println(sekundy + "sekund to " + min2+ "minut." );}
-
-
-
-
-
-
-
-
+        System.out.print("Podaj ilość sekund: ");
+        int sekund = klawiatura.nextInt();
+        if (sekund>=60) {
+            System.out.println("Liczba minut wynosi: "+ sekund/60 + ".");
+        }
+        if (sekund>=3600) {
+            System.out.println("Liczba godzin wynosi: "+ sekund/3600 + ".");
+        }
+        if (sekund>=86400) {
+            System.out.println("Liczba dni wynosi: "+ sekund/86400 + ".");
+        }
+        if (sekund<60){
+            System.out.println("Liczba sekund wynosi: "+ sekund +".");
+        }
+        }
     }
-}
