2e8051d7cd65a406bfdc7b10dc03e006b55cd50b
matecus <125434606+matecus@users.noreply.github.com>
2023-03-07 07:34:42 +0100

    Update Petle3.java

--- src/Plik3/Petle3.java
+++ src/Plik3/Petle3.java
@@ -1,4 +1,24 @@
 package Plik3;
+import java.util.Scanner;
 
 public class Petle3 {
+    public static void main(String[] args) {
+        Scanner klawiatura = new Scanner(System.in);
+        String tekst = "";
+
+        do {
+            System.out.print("Proszę wpisać Tekst 'Tak' lub 'Nie': ");
+            tekst = klawiatura.nextLine();
+
+            if (tekst.equalsIgnoreCase("Tak")) {
+                System.out.println("Wpisany tekst to Tak.");
+            } else if (tekst.equalsIgnoreCase("Nie")) {
+                System.out.println("Wpisany tekst to Nie.");
+            } else {
+                System.out.println("Niepoprawny tekst, proszę spróbować ponownie.");
+            }
+        } while (!tekst.equalsIgnoreCase("Tak") && !tekst.equalsIgnoreCase("Nie"));
+
+        System.out.println("Wpisany tekst jest poprawny.");
+    }
 }
