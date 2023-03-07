0267766022c35f3be07fcf008d46ce9a2f694f68
matecus <125434606+matecus@users.noreply.github.com>
2023-03-07 07:37:22 +0100

    Update Petle4.java

--- src/Plik3/Petle4.java
+++ src/Plik3/Petle4.java
@@ -1,4 +1,12 @@
 package Plik3;
 
+import java.util.Scanner;
+
 public class Petle4 {
+    public static void main(String[] args) {
+
+
+for (int i = 1; i <= 10; i++) {
+    System.out.println("Mateusz");}
+    }
 }
