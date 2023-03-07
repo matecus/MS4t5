dbdac330d4f900326175b9ae603cca3ff9418279
matecus <125434606+matecus@users.noreply.github.com>
2023-03-07 08:04:55 +0100

    Create Petla13.java

---
+++ src/Plik3/Petla13.java
@@ -0,0 +1,10 @@
+public class Petle13 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 15; j++) {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+    }
+}
