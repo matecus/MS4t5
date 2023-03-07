50ca383466d4998c9b0909ef954ed97b97fb942d
matecus <125434606+matecus@users.noreply.github.com>
2023-03-07 07:39:45 +0100

    Update Petle5.java

--- src/Plik3/Petle5.java
+++ src/Plik3/Petle5.java
@@ -1,4 +1,10 @@
-package Plik3;
+import java.util.Scanner;
 
 public class Petle5 {
+    public static void main(String[] args) {
+
+
+for (int i = 1; i <= 49; i += 2) {
+    System.out.println(i);}
+    }
 }
