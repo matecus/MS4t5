95c2bb5fc5ad0237d8e7dd1422999fc22a512da6
matecus <125434606+matecus@users.noreply.github.com>
2023-03-07 07:44:07 +0100

    Create Petle7.java

---
+++ src/Plik3/Petle7.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Petle7 {
+    public static void main(String[] args) {
+        Scanner klawiatura = new Scanner(System.in);
+        int suma = 0;
+        
+        for (int i = 1; i <= 7; i++) {
+            System.out.print("Podaj liczbę: ");
+            int liczba = klawiatura.nextInt();
+            suma += liczba;
+        }
+        
+        System.out.println("Suma wprowadzonych liczb: " + suma);
+    }
+}
