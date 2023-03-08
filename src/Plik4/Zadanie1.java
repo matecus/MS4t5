3a845db97424040f150caa38da13d3f5f6856dd3
matecus <125434606+matecus@users.noreply.github.com>
2023-03-08 17:56:57 +0100

    Update Zadanie1.java

--- src/Plik4/Zadanie1.java
+++ src/Plik4/Zadanie1.java
@@ -1,4 +1,21 @@
 package Plik4;
+import java.util.Scanner;
 
 public class Zadanie1 {
+    public static void main(String[] args) {
+        Scanner klawiatura = new Scanner(System.in);
+        int liczba;
+        do {
+            System.out.print("Podaj dodatnią niezerową liczbę całkowitą: ");
+            liczba = klawiatura.nextInt();
+        } while (liczba <= 0);
+
+        int suma = 0;
+        int i = 1;
+        while (i <= liczba) {
+            suma += i;
+            i++;
+        }
+        System.out.println("Suma liczb od 1 do " + n + " wynosi: " + suma);
+    }
 }
