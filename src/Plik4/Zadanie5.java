7fb1412514035493fd18c78360c83466bb88bb9c
matecus <125434606+matecus@users.noreply.github.com>
2023-03-08 18:13:21 +0100

    Update Zadanie5.java

--- src/Plik4/Zadanie5.java
+++ src/Plik4/Zadanie5.java
@@ -1,1 +1,21 @@
+package Plik4;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner klawiatura = new Scanner(System.in);
+        System.out.println("Podaj łańcuch znaków:");
+        String lancuch = klawiatura.nextLine();
+        System.out.println("Podaj znak:");
+        char znak = klawiatura.nextLine().charAt(0);
+
+        int licznik = 0;
+        for (int i = 0; i < lancuch.length(); i++) {
+            if (lancuch.charAt(i) == znak) {
+                licznik++;
+            }
+        }
+
+        System.out.println("Liczba wystąpień znaku " + znak + " w łańcuchu \"" + lancuch + "\" wynosi: " + licznik);
+    }
+}
